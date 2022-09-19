e757e74de55d94f205fd1b75ee28d2d51c56cbb4
Cecilia Blankson Amponsah <amponsahcecilia77@gmail.com>
2022-09-19 14:25:57 +0000

    first commit

---
+++ math.test.js
@@ -0,0 +1,36 @@
+const { divide, multiply, subtract, sum } = require("./math");
+
+
+describe("Math", () => {
+    
+it("should sum 2 + 2 to be 4", () => {
+  const result = sum(2, 2);
+  expect(result).toBe(4);
+  expect(result).toBeGreaterThan(3);
+});
+
+it("should multiply 3 * 3 to result to 9", () => {
+  const result = multiply(3, 3);
+  expect(result).toBe(9);
+});
+
+it("should subtract 4 - 2 to be result to 2", () => {
+  const result = subtract(4, 2);
+  expect(result).toBe(2);
+});
+
+it("should be false", () => {
+  expect(undefined).toBeFalsy();
+});
+
+it("should divide 4 / 4 to result to 1 ", () => {
+  const result = divide(4, 4);
+  expect(result).toBe(1);
+});
+});
+test("Array", () => {
+  let days = ["Monday", "Tuesday", " Wednesday"];
+  expect(days).toContain("Tuesday");
+  expect(days).not.toContain("Friday");
+});
+
